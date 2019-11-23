cac5101e6c7c5bd2d7f90ad0207c802d0dd4483c
SKTT1Ryze <1276675421@qq.com>
2019-11-23 23:03:55 +0800

    add BiTree experiment file

---
+++ BinaryTree_Link/BinaryTree_Link/BinaryTree_Link.cpp
@@ -0,0 +1,265 @@
+﻿#include "pch.h"
+#include <iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include"BinaryTree_Link.h"
+
+//Functions Declaration
+status CreateBiTree(BinaryTreePos&T, int definition);
+status DestroyBiTree(BinaryTreePos&T);
+status ClearBiTree(BinaryTreePos&T);
+status BiTreeEmpty(BinaryTreePos&T);
+int BiTreeDepth(BinaryTreePos&T);
+BiTPos LocateNode(BinaryTreePos&T, KeyType e);
+status Assign(BinaryTreePos&T, KeyType e, ElemType value);
+BiTPos GetSibling(BinaryTreePos&T, KeyType e);
+status InsertNode(BinaryTreePos&T, KeyType e, int LR, BiTPos&c);
+status DeleteNode(BinaryTreePos&T, KeyType e);
+status PreOrderTraverse(BinaryTreePos&T, void(*visit)(BiTPos&node));
+status InOrderTraverse(BinaryTreePos&T, void(*visit)(BiTPos&node));
+status PostOrderTraverse(BinaryTreePos&T, void(*visit)(BiTPos&node));
+status LevelOrderTraverse(BinaryTreePos&T, void(*visit)(BiTPos&node));
+
+int main(int argc, char *argv[])
+{
+	int op = 1;
+	int index;
+
+
+	while (op)
+	{
+		system("cls");
+		printf("\n\n");
+		printf("      Menu for Linear Table On Sequence Structure \n");
+		printf("------index of present LinearList:%d--------------\n", index);
+		printf("-------------------------------------------------\n");
+		printf("    	  1. CreateBiTree      2. DestroyBiTree\n");
+		printf("    	  3. ClearBiTree       4. BiTreeEmpty\n");
+		printf("    	  5. BiTreeDepth       6. LocateNode\n");
+		printf("    	  7. Assign            8. GetSibling\n");
+		printf("    	  9. InsertNode        10.DeleteNode\n");
+		printf("    	  11.PreOrderTraverse  12.InOrderTraverse\n");
+		printf("    	  13.PostOrderTraverse 14.LevelOrderTraverse\n");
+		printf("    	  15.SaveBiTree        16.LoadBiTree\n");
+		printf("          17.ChangeBiTree      0. Exit\n");
+		printf("-------------------------------------------------\n");
+		printf("    请选择你的操作[0~17]:");
+		scanf_s("%d", &op);
+		switch (op)
+		{
+			case 1:
+				printf("Your choise:1\n");
+				printf("/*\n *Function Name:CreateBiTree\n");
+				printf(" *Module:Data structures\n");
+				printf(" *Parameter:LinkList L\n");
+				printf(" *Return:status\n");
+				printf(" *Use:initialize the LinearList\n*/\n");
+				
+				getchar();
+				getchar();
+				break;
+			case 2:
+				printf("Your choise:2\n");
+				printf("/*\n *Function Name:DestroyBiTree\n");
+				printf(" *Module:Data structures\n");
+				printf(" *Parameter:LinkList L\n");
+				printf(" *Return:status\n");
+				printf(" *Use:destroy the LinearList\n*/\n");
+				
+				getchar();
+				getchar();
+				break;
+			case 3:
+				printf("Your choise:3\n");
+				printf("/*\n *Function Name:ClearBiTree\n");
+				printf(" *Module:Data structures\n");
+				printf(" *Parameter:LinkList L\n");
+				printf(" *Return:status\n");
+				printf(" *Use:reset the LinearList\n*/\n");
+				
+				getchar();
+				getchar();
+				break;
+			case 4:
+				printf("Your choise:4\n");
+				printf("/*\n *Function Name:BiTreeEmpty\n");
+				printf(" *Module:Data structures\n");
+				printf(" *Parameter:LinkList L\n");
+				printf(" *Return:status\n");
+				printf(" *Use:judge the LinearList null or not\n*/\n");
+				
+				getchar();
+				getchar();
+				break;
+			case 5:
+				printf("Your choise:5\n");
+				printf("/*\n *Function Name:BiTreeDepth\n");
+				printf(" *Module:Data structures\n");
+				printf(" *Parameter:LinkList L\n");
+				printf(" *Return:int\n");
+				printf(" *Use:return the length of the LinearList\n*/\n");
+				
+				getchar();
+				getchar();
+				break;
+			case 6:
+				printf("Your choise:6\n");
+				printf("/*\n *Function Name:LocateNode\n");
+				printf(" *Module:Data structures\n");
+				printf(" *Parameter:LinkList L,int order,ElemType& elem\n");
+				printf(" *Return:status\n");
+				printf(" *Use:get the No.order element of the LinearList\n*/\n");
+				
+				getchar();
+				getchar();
+				break;
+			case 7:
+				printf("Your choise:7\n");
+				printf("/*\n *Function Name:Assign\n");
+				printf(" *Module:Data structures\n");
+				printf(" *Parameter:LinkList L,ElemType elem,FunctionPointer compare\n");
+				printf(" *Return:int\n");
+				printf(" *Use:find the first element in the LinearList\n");
+				printf(" *with the relationship compare with e\n*/\n");
+				
+				getchar();
+				getchar();
+				break;
+			case 8:
+				printf("Your choise:8\n");
+				printf("/*\n *Function Name:GetSibling\n");
+				printf(" *Module:Data structures\n");
+				printf(" *Parameter:LinkList L,ElemType cur,ElemType&pre_e\n");
+				printf(" *Return:status\n");
+				printf(" *Use:find the pre element of cur in the LinearList\n*/\n");
+				
+				getchar();
+				getchar();
+				break;
+			case 9:
+				printf("Your choise:9\n");
+				printf("/*\n *Function Name:InsertNode\n");
+				printf(" *Module:Data structures\n");
+				printf(" *Parameter:LinkList L,ElemType cur,ElemType&next_e\n");
+				printf(" *Return:status\n");
+				printf(" *Use:find the next element of cur in the LinearList\n*/\n");
+				
+				getchar();
+				getchar();
+				break;
+			case 10:
+				printf("Your choise:10\n");
+				printf("/*\n *Function Name:DeleteNode\n");
+				printf(" *Module:Data structures\n");
+				printf(" *Parameter:LinkList L,int order,ElemType elem\n");
+				printf(" *Return:status\n");
+				printf(" *Use:insert an element into the LinearList\n*/\n");
+				
+				getchar();
+				getchar();
+				break;
+			case 11:
+				printf("Your choise:11\n");
+				printf("/*\n *Function Name:PreOrderTraverse\n");
+				printf(" *Module:Data structures\n");
+				printf(" *Parameter:LinkList L,int order,ElemType& elem\n");
+				printf(" *Return:status\n");
+				printf(" *Use:delete the No.order element of the LinearList\n*/\n");
+				
+				getchar();
+				getchar();
+				break;
+			case 12:
+				printf("Your choise:12\n");
+				printf("/*\n *Function Name:InOrderTraverse\n");
+				printf(" *Module:Data structures\n");
+				printf(" *Parameter:LinkList L,FunctionPointer visit\n");
+				printf(" *Return:status\n");
+				printf(" *Use:travel the whole LinearList\n*/\n");
+				
+				getchar();
+				getchar();
+				break;
+			case 13:
+				printf("Your choise:13\n");
+				printf("/*\n");
+				printf(" *Function Name:PostOrderTraverse\n");
+				printf(" *Module:Data structures\n");
+				printf(" *Use:save data in the LinearList as a file\n");
+				
+				getchar();
+				getchar();
+				break;
+			case 14:
+				printf("Your choise:14\n");
+				printf("/*\n");
+				printf(" *Function Name:LevelOrderTraverse\n");
+				printf(" *Module:Data structures\n");
+				printf(" *Use:read data in the LinearList as a file\n");
+				
+				getchar();
+				getchar();
+				break;
+			case 15:
+				printf("Your choise:15\n");
+				printf("/*\n *Function Name:SaveBiTree\n");
+				printf(" *Module:Data structures\n");
+				printf(" *Parameter:LinkList L\n");
+				printf(" *Return:status\n");
+				printf(" *Use:sort the LinearList\n*/\n");
+				
+				getchar();
+				getchar();
+				break;
+			case 16:
+				printf("Your choise:16\n");
+				printf("/*\n *Function Name:LoadBiTree\n");
+				printf(" *Module:Data structures\n");
+				printf(" *Use:change the LinearList\n*/\n");
+				
+				getchar();
+				getchar();
+				break;
+			case 17:
+				printf("Your choise:17\n");
+				printf("/*\n *Function Name:ChangeBiTree\n");
+				printf(" *Module:Data structures\n");
+				printf(" *Parameter:LinkList La,LinkList Lb,Linklist Lc\n");
+				printf(" *Return:status\n");
+				printf(" *Use:merge two LinearList as new one\n*/\n");
+				
+				getchar();
+				getchar();
+				break;
+			case 0:
+				break;
+		}//end of switch
+		printf("Welcome next time");
+	}//end of while
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//end of main
+
---
+++ BinaryTree_Link/BinaryTree_Link/StackADT.h
@@ -0,0 +1,97 @@
+#pragma once
+//Stack ADT
+#include<stdio.h>
+#include<malloc.h>
+#include<stdlib.h>
+#include"BinaryTree_Link.h"
+//define return value
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASTABLE -1
+#define OVERFLOW -2
+#define STACK_INIT_SIZE 100
+#define STACKINCREMENT 10
+
+typedef int status;
+typedef BiTPos SElemType;//type of data
+
+typedef struct//define of Stack Struct
+{
+	SElemType *base;
+	SElemType *top;
+	int stacksize;
+}SqStack;
+
+status InitStack(SqStack&S);
+status GetTop(SqStack&S, SElemType&e);
+status Push(SqStack&S, SElemType e);
+status Pop(SqStack&S, SElemType&e);
+
+status InitStack(SqStack&S)
+{
+	//构造一个空栈
+	S.base = (SElemType*)malloc(STACK_INIT_SIZE * sizeof(SElemType));
+	if (!S.base)exit(OVERFLOW);
+	S.top = S.base;
+	S.stacksize = STACK_INIT_SIZE;
+	return OK;
+}//InitStack
+status StackEmpty(SqStack&S)
+{
+	if (S.base == S.top)
+	{
+		printf("*Empty\n");
+		return TRUE;
+	}
+	else
+	{
+		printf("*Not Empty\n");
+		return FALSE;
+	}
+}
+
+
+status GetTop(SqStack&S, SElemType&e)
+{
+	//若栈不为空，则用e返回S的栈顶元素，并返回OK，否则返回ERROR		
+	if (S.top == S.base)
+	{
+		printf("*Stack is Empty\n");
+		return ERROR;
+	}
+	e = *(S.top - 1);
+	return OK;
+}//GetTop
+
+status Push(SqStack&S, SElemType e)
+{
+	//插入元素e为新的栈顶元素
+	if (S.top - S.base >= S.stacksize)
+	{
+		//栈满，追加存储空间
+		S.base = (SElemType*)realloc(S.base, (S.stacksize + STACKINCREMENT) * sizeof(SElemType));
+		if (!S.base)exit(OVERFLOW);
+		S.top = S.base + S.stacksize;
+		S.stacksize += STACKINCREMENT;
+	}
+	*S.top++ = e;
+	return OK;
+}//Push
+
+status Pop(SqStack&S, SElemType&e)
+{
+	//若栈不空，则删除S的栈顶元素，用e返回其值，并返回OK，否则返回ERROR
+	if (S.top == S.base)
+	{
+		printf("*Stack is Empty\n");
+		return ERROR;
+	}
+	e = *--S.top;
+	return OK;
+}//Pop
+
+
+
+

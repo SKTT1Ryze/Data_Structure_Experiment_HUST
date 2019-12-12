db5ca159f15aa5f8b5a354c50ca164a305fc751f
SKTT1Ryze <1276675421@qq.com>
2019-12-12 16:22:02 +0800

    change to dev

---
+++ Graph_dev/Graph.cpp
@@ -0,0 +1,486 @@
+#include <iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include"GraphADT.h"
+//define return value
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASTABLE -1
+#define max_vex 100
+#define max_arc 200
+#define max_graph 10
+//Functions Declaration
+status CreateGraph(GPos&G, int*vex, int*vex_arc);
+status DestroyGraph(GPos&G);
+int LocateVex(GPos&G, int u);
+status PutVex(GPos&G, int u, ElemType value);
+int FirstAdjVex(GPos&G, int u);
+int NextAdjVex(GPos&G, int v, int w);
+status InsertVex(GPos&G, vexnode*v);
+status DeleteVex(GPos&G, int v);
+status InsertArc(GPos&G, int v, int w);
+status DeleteArc(GPos&G, int v, int w);
+status DFSTraverse(GPos&G, void(*visit)(vexnode*vex));
+status BFSTraverse(GPos&G, void(*visit)(vexnode*vex));
+status print_AdjList(GPos&G);
+void DeleteLink(arcnode*head);
+void DFS(GPos&G, vexnode*vex, void(*visit)(vexnode*vex));
+void visit(vexnode*vex);
+
+
+
+
+int main(int argc,char *argv[])
+{
+	int op = 1;
+	int index = 0;
+	int vex[max_vex];
+	int vex_arc[max_arc];
+	int i = 0;
+	int vex_temp = 0;
+	int arc_temp = 0;
+	GPos GraphGroup[max_graph];
+	for (i = 0; i < max_graph; i++)
+	{
+		GraphGroup[i] = (GPos)malloc(sizeof(G));
+		if (!GraphGroup[i])
+		{
+			printf("*overflow\n");
+			exit(OVERFLOW);
+		}
+		GraphGroup[i]->arc_num = 0;
+		GraphGroup[i]->first_vex = NULL;
+		GraphGroup[i]->id = i;
+		GraphGroup[i]->vex_num = 0;
+	}
+	GPos *G = &GraphGroup[0];
+
+	for (i = 0; i < max_vex; i++)
+		vex[i] = 0;
+	for (i = 0; i < max_arc; i++)
+		vex_arc[i] = 0;
+
+	int u = 0;
+	int v = 0;
+	int w = 0;
+	int location = 0;
+	int input_value = 0;
+	vexnode*vexnode_temp = NULL;
+	while (op)
+	{
+		system("cls");
+		printf("\n\n");
+		printf("      Menu for Linear Table On Sequence Structure \n");
+		printf("------------index of present Graph:%d-------------\n", index);
+		printf("-------------------------------------------------\n");
+		printf("    	  1. CreateGraph      2. DestroyGraph\n");
+		printf("    	  3. LocateVex        4. PutVex\n");
+		printf("    	  5. FirstAdjVex      6. NextAdjVex \n");
+		printf("    	  7. InsertVex        8. DeleteVex\n");
+		printf("    	  9. InsertArc        10. DeleteArc\n");
+		printf("    	  11. DFSTraverse     12. BFSTraverse\n");
+		printf("    	  13. save            14. load\n");
+		printf("    	  15. print_AdjList   16.      \n");
+		printf("          17.                 0. Exit\n");
+		printf("          -1.dir\n");
+		printf("-------------------------------------------------\n");
+		printf("    ÇëÑ¡ÔñÄãµÄ²Ù×÷[-1~17]:");
+		scanf("%d", &op);
+		switch (op)
+		{
+		case 1:
+			printf("Your choise:1\n");
+			printf("/*\n *Function Name:CreateGraph\n");
+			printf(" *Module:Data structures\n");
+			printf(" *Use:Create a Graph\n*/\n");
+			printf("*input vex set with positive int,end with -1\n");
+			i = 0;
+			do
+			{
+				scanf("%d", &vex_temp);
+				if (vex_temp == -1)
+				{
+					printf("*input end\n");
+					vex[i++] = vex_temp;
+					break;
+				}
+				if (vex_temp <= 0)
+				{
+					printf("*input error\n");
+					continue;
+				}
+				vex[i++] = vex_temp;
+				if (i >= max_vex - 1)
+				{
+					printf("*input overflow\n");
+					break;
+				}
+			} while (1);
+			i = 0;
+			printf("*input vex_arc set with positive int,end with -1\n");
+			while (1)
+			{
+				scanf("%d", &arc_temp);
+				if (arc_temp == -1)
+				{
+					printf("*input end\n");
+					vex_arc[i++] = arc_temp;
+					break;
+				}
+				if (arc_temp <= 0)
+				{
+					printf("*input error\n");
+					continue;
+				}
+				vex_arc[i++] = arc_temp;
+				if (i >= max_arc - 1)
+				{
+					printf("*input overflow\n");
+					break;
+				}
+			}
+			if (CreateGraph(*G, vex, vex_arc) == OK)
+			{
+				printf("*Operator Success\n");
+			}
+			else
+			{
+				printf("*Operator ERROR\n");
+			}
+			getchar();
+			getchar();
+			break;
+		case 2:
+			printf("Your choise:2\n");
+			printf("/*\n *Function Name:DestroyGraph\n");
+			printf(" *Module:Data structures\n");
+			printf(" *Use:destroy the Graph\n*/\n");
+			if (DestroyGraph(*G) == OK)
+			{
+				printf("*Operator Success\n");
+			}
+			else
+			{
+				printf("*Operator ERROR\n");
+			}
+			getchar();
+			getchar();
+			break;
+		case 3:
+			printf("Your choise:3\n");
+			printf("/*\n *Function Name:LocateVex\n");
+			printf(" *Module:Data structures\n");
+			printf(" *Use:locate the vex node\n*/\n");
+			printf("*input u:");
+			scanf("%d", &u);
+			if (u <= 0)
+			{
+				printf("*input error\n");
+				getchar();
+				getchar();
+				break;
+			}
+			location = LocateVex(*G, u);
+			printf("*Location:%d\n", location);
+			getchar();
+			getchar();
+			break;
+		case 4:
+			printf("Your choise:4\n");
+			printf("/*\n *Function Name:PutVex\n");
+			printf(" *Module:Data structures\n");
+			printf(" *Use:Assign the value of the vex node\n*/\n");
+			printf("*input u:");
+			scanf("%d", &u);
+			if (u <= 0)
+			{
+				printf("*input error\n");
+				getchar();
+				getchar();
+				break;
+			}
+			printf("*input value:");
+			scanf("%d", &input_value);
+			if (PutVex(*G, u, input_value) == OK)
+			{
+				printf("*Operator Success\n");
+			}
+			else
+			{
+				printf("*Operator Error\n");
+			}
+			getchar();
+			getchar();
+			break;
+		case 5:
+			printf("Your choise:5\n");
+			printf("/*\n *Function Name:FirstAdjVex\n");
+			printf(" *Module:Data structures\n");
+			printf(" *Use:return first adjvex of the vex node\n*/\n");
+			printf("*input u:");
+			scanf("%d", &u);
+			if (u <= 0)
+			{
+				printf("*input error\n");
+				getchar();
+				getchar();
+				break;
+			}
+			location = LocateVex(*G, u);
+			printf("*Location:%d\n", location);
+			getchar();
+			getchar();
+			break;
+		case 6:
+			printf("Your choise:6\n");
+			printf("/*\n *Function Name:NextAdjVex\n");
+			printf(" *Module:Data structures\n");
+			printf(" *Use:get the next adjvex\n*/\n");
+			printf("*input v:");
+			scanf("%d", &v);
+			if (v <= 0)
+			{
+				printf("*input error\n");
+				getchar();
+				getchar();
+				break;
+			}
+			printf("*input w:");
+			scanf("%d", &w);
+			if (w <= 0)
+			{
+				printf("*input error\n");
+				getchar();
+				getchar();
+				break;
+			}
+			location = NextAdjVex(*G, v, w);
+			printf("*Location:%d\n", location);
+			getchar();
+			getchar();
+			break;
+		case 7:
+			printf("Your choise:7\n");
+			printf("/*\n *Function Name:InsertVex\n");
+			printf(" *Module:Data structures\n");
+			printf(" *Use:Insert a vex node\n");
+			vexnode_temp = (vexnode*)malloc(sizeof(vexnode));
+			if (vexnode_temp == NULL)
+			{
+				printf("*overflow\n");
+				exit(OVERFLOW);
+			}
+			vexnode_temp->first_arc = NULL;
+			vexnode_temp->index = 0;
+			vexnode_temp->nextvex = NULL;
+			vexnode_temp->value = 0;
+			printf("*input index:");
+			scanf("%d", &(vexnode_temp->index));
+			printf("*input value:");
+			scanf("%d", &(vexnode_temp->value));
+			if (InsertVex(*G, vexnode_temp) == OK)
+			{
+				printf("*Operatoe Success\n");
+			}
+			else
+			{
+				printf("*Operator Error\n");
+			}
+			getchar();
+			getchar();
+			break;
+		case 8:
+			printf("Your choise:8\n");
+			printf("/*\n *Function Name:DeleteVex\n");
+			printf(" *Module:Data structures\n");
+			printf(" *Use:Delete a vex node\n*/\n");
+			printf("*input v:");
+			scanf("%d", &v);
+			if (v <= 0)
+			{
+				printf("*input error\n");
+				getchar();
+				getchar();
+				break;
+			}
+			if (DeleteVex(*G, v) == OK)
+			{
+				printf("*Operator Success\n");
+			}
+			else
+			{
+				printf("*Operator Error\n");
+			}
+			getchar();
+			getchar();
+			break;
+		case 9:
+			printf("Your choise:9\n");
+			printf("/*\n *Function Name:InsertArc\n");
+			printf(" *Module:Data structures\n");
+			printf(" *Use:Insert an arc node\n*/\n");
+			printf("*input v:");
+			scanf("%d", &v);
+			if (v <= 0)
+			{
+				printf("*input error\n");
+				getchar();
+				getchar();
+				break;
+			}
+			printf("*input w:");
+			scanf("%d", &w);
+			if (w <= 0)
+			{
+				printf("*input error\n");
+				getchar();
+				getchar();
+				break;
+			}
+			if (InsertArc(*G, v, w) == OK)
+			{
+				printf("*Operator Success\n");
+			}
+			else
+			{
+				printf("*Operator Error\n");
+			}
+			getchar();
+			getchar();
+			break;
+		case 10:
+			printf("Your choise:10\n");
+			printf("/*\n *Function Name:DeleteArc\n");
+			printf(" *Module:Data structures\n");
+			printf(" *Use:Delete an arc node\n*/\n");
+			printf("*input v:");
+			scanf("%d", &v);
+			if (v <= 0)
+			{
+				printf("*input error\n");
+				getchar();
+				getchar();
+				break;
+			}
+			printf("*input w:");
+			scanf("%d", &w);
+			if (w <= 0)
+			{
+				printf("*input error\n");
+				getchar();
+				getchar();
+				break;
+			}
+			if (DeleteArc(*G, v, w) == OK)
+			{
+				printf("*Operator Success\n");
+			}
+			else
+			{
+				printf("*Operator Error\n");
+			}
+			getchar();
+			getchar();
+			break;
+		case 11:
+			printf("Your choise:11\n");
+			printf("/*\n *Function Name:DFSTraverse\n");
+			printf(" *Module:Data structures\n");
+			printf(" *Use:Travel the Graph in DFS order\n*/\n");
+			if (DFSTraverse(*G, visit) == OK)
+			{
+				printf("*Operator Success\n");
+			}
+			else
+			{
+				printf("*Operator Error\n");
+			}
+			getchar();
+			getchar();
+			break;
+		case 12:
+			printf("Your choise:12\n");
+			printf("/*\n *Function Name:BFSTraverse\n");
+			printf(" *Module:Data structures\n");
+			printf(" *Use:Travle the Graph in BFS order\n*/\n");
+			if (BFSTraverse(*G, visit) == OK)
+			{
+				printf("*Operator Success\n");
+			}
+			else
+			{
+				printf("*Operator Error\n");
+			}
+			getchar();
+			getchar();
+			break;
+		case 13:
+			printf("Your choise:13\n");
+			printf("/*\n");
+			printf(" *Function Name:save\n");
+			printf(" *Module:Data structures\n");
+			printf(" *Use:save this Graph as a file\n");
+			
+			getchar();
+			getchar();
+			break;
+		case 14:
+			printf("Your choise:14\n");
+			printf("/*\n");
+			printf(" *Function Name:Load\n");
+			printf(" *Module:Data structures\n");
+			printf(" *Use:Load a Graph from a file\n");
+			printf("*/\n");
+			
+			getchar();
+			getchar();
+			break;
+		case 15:
+			printf("Your choise:15\n");
+			printf("/*\n *Function Name:print_AdjList\n");
+			printf(" *Module:Data structures\n");
+			printf(" *Use:print the AdjList\n*/\n");
+			if (print_AdjList(*G) == OK)
+			{
+				printf("*Operator Success\n");
+			}
+			else
+			{
+				printf("*Operator Error\n");
+			}
+			getchar();
+			getchar();
+			break;
+		case 16:
+			printf("Your choise:16\n");
+			printf("/*\n *Function Name:ChangeList\n");
+			printf(" *Module:Data structures\n");
+			printf(" *Use:change the LinearList\n*/\n");
+			
+			getchar();
+			getchar();
+			break;
+		case 17:
+			printf("Your choise:17\n");
+			printf("/*\n *Function Name:\n");
+			printf(" *Module:Data structures\n");
+			printf(" *Use:\n*/\n");
+			
+			getchar();
+			getchar();
+			break;
+		case 0:
+			break;
+		case -1:
+			
+			getchar();
+			getchar();
+			break;
+
+		}//end of switch
+		printf("Welcome next time");
+	}//end of while
+
+}//end of main function
---
+++ Graph_dev/GraphADT.h
@@ -0,0 +1,782 @@
+//Graph head file
+#include<stdio.h>
+#include<malloc.h>
+#include<stdlib.h>
+//define return value
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASTABLE -1
+#define OVERFLOW -1
+typedef int status;
+typedef int ElemType;//type of data
+
+
+
+typedef struct ArcNode
+{
+	int vex_index;
+	struct ArcNode *nextarc;
+	int weight;
+}arcnode;
+
+typedef struct VexNode
+{
+	int index;
+	ElemType value;
+	arcnode *first_arc;
+	struct VexNode	*nextvex;
+}vexnode;
+
+typedef struct Graph
+{
+	int id;
+	vexnode *first_vex;
+	int vex_num;
+	int arc_num;
+}G,*GPos;
+
+int visited[100];
+vexnode*Queue[100];
+//Functions Declaration
+status CreateGraph(GPos&G, int*vex, int*vex_arc);
+status DestroyGraph(GPos&G);
+int LocateVex(GPos&G, int u);
+status PutVex(GPos&G, int u, ElemType value);
+int FirstAdjVex(GPos&G, int u);
+int NextAdjVex(GPos&G, int v, int w);
+status InsertVex(GPos&G, vexnode*v);
+status DeleteVex(GPos&G, int v);
+status InsertArc(GPos&G, int v, int w);
+status DeleteArc(GPos&G, int v, int w);
+status DFSTraverse(GPos&G, void(*visit)(vexnode*vex));
+status BFSTraverse(GPos&G, void(*visit)(vexnode*vex));
+status print_AdjList(GPos&G);
+void DeleteLink(arcnode*head);
+void DFS(GPos&G, vexnode*vex, void(*visit)(vexnode*vex));
+void BFS(GPos&G, void(*visit)(vexnode*vex));
+void visit(vexnode*vex);
+
+
+status CreateGraph(GPos&G, int*vex, int*vex_arc)
+{
+	//Graph NULL
+	if (G == NULL)
+	{
+		printf("*The Graph struct is NULL\n");
+		return ERROR;
+	}
+	if (G->first_vex != NULL)
+	{
+		printf("*The Graph is not NULL\n");
+		return ERROR;
+	}
+	int vex_num = 0;
+	int arc_num = 0;
+	int i = 0;
+	int *pos = vex;
+	vexnode* vex_temp = NULL;
+	vexnode* vex_travel = NULL;
+	arcnode* arc_travel = NULL;
+	arcnode* arc_temp = NULL;
+	while (pos[i++]!= -1)
+	{
+		vex_num++;
+		//pos++;
+	}
+	printf("*vex num:%d\n", vex_num);
+	G->vex_num = vex_num;
+	pos = vex_arc;
+	i = 0;
+	while (pos[i] != -1)
+	{
+		arc_num++;
+		i += 2;
+		//pos += 2;
+	}
+	printf("*arc num:%d\n", arc_num);
+	G->arc_num = arc_num;
+	if (vex_num == 0 || arc_num == 0)
+	{
+		printf("*input null\n");
+		return ERROR;
+	}
+	i = 0;
+	vex_temp = (vexnode*)malloc(sizeof(vexnode));
+	if (!vex_temp)
+	{
+		printf("*overflow\n");
+		exit(OVERFLOW);
+	}
+	vex_temp->index = *(vex + i);
+	vex_temp->value = 0;
+	vex_temp->first_arc = NULL;
+	vex_temp->nextvex = NULL;
+	G->first_vex = vex_temp;
+	vex_travel = G->first_vex;
+	for (i = 1; i < vex_num; i++)
+	{
+		vex_temp = (vexnode*)malloc(sizeof(vexnode));
+		if (!vex_temp)
+		{
+			printf("*overflow\n");
+			exit(OVERFLOW);
+		}
+		vex_temp->index = *(vex + i);
+		vex_temp->value = 0;
+		vex_temp->first_arc = NULL;
+		vex_temp->nextvex = NULL;
+		vex_travel->nextvex = vex_temp;
+		vex_travel = vex_travel->nextvex;
+	}
+	vex_travel = G->first_vex;
+	while (vex_travel)
+	{
+		printf("index:%d,value:%d\n", vex_travel->index, vex_travel->value);
+		vex_travel = vex_travel->nextvex;
+	}
+	printf("*vex create success\n");
+	int vex_index = 0;
+	int arc_index = 0;
+	for (i = 0; i < arc_num; i++)
+	{
+		vex_index = *(vex_arc + i * 2);
+		arc_index = *(vex_arc + i * 2 + 1);
+		printf("vex_index:%d,arc_index:%d\n", vex_index, arc_index);
+		vex_travel = G->first_vex;
+		while (vex_travel&&vex_travel->index != vex_index)
+			vex_travel = vex_travel->nextvex;
+		if (!vex_travel)
+		{
+			printf("*input error,VR has V not in V\n");
+			return ERROR;
+		}
+		if (vex_travel->first_arc == NULL)
+		{
+			printf("*the first arc\n");
+			vex_travel->first_arc = (arcnode*)malloc(sizeof(arcnode));
+			if (vex_travel->first_arc == NULL)
+			{
+				printf("*overflow\n");
+				exit(OVERFLOW);
+			}
+			vex_travel->first_arc->vex_index = arc_index;
+			vex_travel->first_arc->nextarc = NULL;
+			vex_travel->first_arc->weight = 1;
+			printf("*first arc create success\n");
+		}
+		else
+		{
+			arc_travel = vex_travel->first_arc;
+			while (arc_travel&&arc_travel->nextarc)
+				arc_travel = arc_travel->nextarc;
+			arc_temp = (arcnode*)malloc(sizeof(arcnode));
+			if (arc_temp == NULL)
+			{
+				printf("*overflow\n");
+				exit(OVERFLOW);
+			}
+			arc_temp->vex_index = arc_index;
+			arc_temp->nextarc = NULL;
+			arc_temp->weight = 1;
+			arc_travel->nextarc = arc_temp;
+			printf("*arc create success\n");
+		}
+	}
+	//Travel
+	printf("*Graph create success\n");
+	return OK;
+}
+
+status DestroyGraph(GPos&G)
+{
+	//Graph NULL
+	if (G == NULL)
+	{
+		printf("*The Graph struct is NULL\n");
+		return ERROR;
+	}
+	if (G->first_vex == NULL)
+	{
+		printf("*The Graph is NULL\n");
+		return ERROR;
+	}
+	vexnode*vex_pos = G->first_vex;
+	while (vex_pos)
+	{
+		DeleteLink(vex_pos->first_arc);
+		vex_pos->first_arc = NULL;
+		vex_pos = vex_pos->nextvex;
+	}
+	vex_pos = G->first_vex;
+	vexnode* vex_temp = NULL;
+	do
+	{
+		vex_temp = vex_pos->nextvex;
+		free(vex_pos);
+		vex_pos = vex_temp;
+	} while (vex_pos);
+	G->first_vex = NULL;
+	printf("*Destroy success\n");
+	return OK;
+}
+
+int LocateVex(GPos&G, int u)
+{
+	//Graph NULL
+	if (G == NULL)
+	{
+		printf("*The Graph struct is NULL\n");
+		return -1;
+	}
+	if (G->first_vex == NULL)
+	{
+		printf("*The Graph is NULL\n");
+		return -1;
+	}
+	int i = 0;
+	vexnode*vex_pos = G->first_vex;
+	while (vex_pos&&vex_pos->index != u)
+	{
+		vex_pos = vex_pos->nextvex;
+		i++;
+	}
+	if (!vex_pos)
+	{
+		printf("*Not Found\n");
+		return -1;
+	}
+	printf("*Found\n");
+	return i;
+}
+
+status PutVex(GPos&G, int u, ElemType value)
+{
+	//Graph NULL
+	if (G == NULL)
+	{
+		printf("*The Graph struct is NULL\n");
+		return ERROR;
+	}
+	if (G->first_vex == NULL)
+	{
+		printf("*The Graph is NULL\n");
+		return ERROR;
+	}
+	vexnode*vex_pos = G->first_vex;
+	while (vex_pos&&vex_pos->index != u)
+		vex_pos = vex_pos->nextvex;
+	if (!vex_pos)
+	{
+		printf("*Not Found\n");
+		return ERROR;
+	}
+	printf("*Found\n");
+	vex_pos->value = value;
+	printf("*Assign success:%d\n", value);
+	return OK;
+}
+
+int FirstAdjVex(GPos&G, int u)
+{
+	//Graph NULL
+	if (G == NULL)
+	{
+		printf("*The Graph struct is NULL\n");
+		return -1;
+	}
+	if (G->first_vex == NULL)
+	{
+		printf("*The Graph is NULL\n");
+		return -1;
+	}
+	vexnode*vex_pos = G->first_vex;
+	while (vex_pos&&vex_pos->index != u)
+		vex_pos = vex_pos->nextvex;
+	if (!vex_pos)
+	{
+		printf("*Not Found u\n");
+		return -1;
+	}
+	printf("*Found u\n");
+	if (vex_pos->first_arc == NULL)
+	{
+		printf("*dont have adj\n");
+		return -1;
+	}
+	int first_adj_index = vex_pos->first_arc->vex_index;
+	vex_pos = G->first_vex;
+	int i = 0;
+	while (vex_pos&&vex_pos->index != first_adj_index)
+	{
+		vex_pos = vex_pos->nextvex;
+		i++;
+	}
+	if (!vex_pos)
+	{
+		printf("*Not Found\n");
+		return -1;
+	}
+	//printf("*Found\n");
+	return i;
+}
+
+int NextAdjVex(GPos&G, int v, int w)
+{
+	//Graph NULL
+	if (G == NULL)
+	{
+		printf("*The Graph struct is NULL\n");
+		return -1;
+	}
+	if (G->first_vex == NULL)
+	{
+		printf("*The Graph is NULL\n");
+		return -1;
+	}
+	vexnode*vex_pos = G->first_vex;
+	while (vex_pos&&vex_pos->index != v)
+		vex_pos = vex_pos->nextvex;
+	if (!vex_pos)
+	{
+		printf("*Not Found v\n");
+		return -1;
+	}
+	printf("*Found v\n");
+	if (vex_pos->first_arc == NULL)
+	{
+		printf("*dont have adj\n");
+		return -1;
+	}
+	arcnode* arc_pos = vex_pos->first_arc;
+	while (arc_pos&&arc_pos->vex_index!=w)
+		arc_pos = arc_pos->nextarc;
+	if (arc_pos == NULL)
+	{
+		printf("*Not Found w\n");
+		return -1;
+	}
+	printf("*Found w\n");
+	if (arc_pos->nextarc == NULL)
+	{
+		printf("*the last adj\n");
+		return -1;
+	}
+	int adj_index = arc_pos->nextarc->vex_index;
+	vex_pos = G->first_vex;
+	int i = 0;
+	while (vex_pos&&vex_pos->index != adj_index)
+	{
+		vex_pos = vex_pos->nextvex;
+		i++;
+	}
+	if (!vex_pos)
+	{
+		printf("*Not Found\n");
+		return -1;
+	}
+	return i;
+}
+
+status InsertVex(GPos&G, vexnode*v)
+{
+	//Graph NULL
+	if (G == NULL)
+	{
+		printf("*The Graph struct is NULL\n");
+		return ERROR;
+	}
+	if (G->first_vex == NULL)
+	{
+		printf("*The Graph is NULL\n");
+		return ERROR;
+	}
+	vexnode*vex_pos = G->first_vex;
+	while (vex_pos->nextvex&&vex_pos)
+		vex_pos = vex_pos->nextvex;
+	vex_pos->nextvex = v;
+	printf("*Insert vex success\n");
+	return OK;
+}
+
+status DeleteVex(GPos&G, int v)
+{
+	//Graph NULL
+	if (G == NULL)
+	{
+		printf("*The Graph struct is NULL\n");
+		return ERROR;
+	}
+	if (G->first_vex == NULL)
+	{
+		printf("*The Graph is NULL\n");
+		return ERROR;
+	}
+	vexnode*vex_pos = G->first_vex;
+	while (vex_pos&&vex_pos->index != v)
+		vex_pos = vex_pos->nextvex;
+	if (vex_pos == NULL)
+	{
+		printf("*Not Found\n");
+		return ERROR;
+	}
+	printf("*Found\n");
+	DeleteLink(vex_pos->first_arc);
+	vex_pos->first_arc = NULL;
+	if (vex_pos==G->first_vex)
+	{
+		//the first vex node
+		printf("*the first vex node\n");
+		G->first_vex = vex_pos->nextvex;
+		free(vex_pos);
+		vex_pos = NULL;
+	}
+	else
+	{
+		vexnode*vex_pre = G->first_vex;
+		while (vex_pre->nextvex&&vex_pre->nextvex->index != v)
+			vex_pre = vex_pre->nextvex;
+		vex_pre->nextvex = vex_pos->nextvex;
+		free(vex_pos);
+		vex_pos = NULL;
+	}
+	vex_pos = G->first_vex;
+	arcnode*arc_pos = NULL;
+	arcnode*arc_pre = NULL;
+	while (vex_pos)
+	{
+		arc_pos = vex_pos->first_arc;
+		while (arc_pos)
+		{
+			if (arc_pos->vex_index == v)
+			{
+				if (arc_pos == vex_pos->first_arc)
+				{
+					//the first arc node
+					printf("*the first arc node\n");
+					vex_pos->first_arc = arc_pos->nextarc;
+					free(arc_pos);
+					arc_pos = vex_pos->first_arc;
+				}
+				else
+				{
+					arc_pre->nextarc = arc_pos->nextarc;
+					free(arc_pos);
+					arc_pos = arc_pre->nextarc;
+				}
+			}
+			arc_pre = arc_pos;
+			arc_pos = arc_pos->nextarc;
+		}
+		vex_pos = vex_pos->nextvex;
+
+		
+	}
+	printf("*Delete vex success\n");
+	return OK;
+}
+
+status InsertArc(GPos&G, int v, int w)
+{
+	//Graph NULL
+	if (G == NULL)
+	{
+		printf("*The Graph struct is NULL\n");
+		return ERROR;
+	}
+	if (G->first_vex == NULL)
+	{
+		printf("*The Graph is NULL\n");
+		return ERROR;
+	}
+	vexnode*vex_pos = G->first_vex;
+	while (vex_pos&&vex_pos->index != w)
+		vex_pos = vex_pos->nextvex;
+	if (!vex_pos)
+	{
+		printf("*the node w dont exist\n");
+		return ERROR;
+	}
+	printf("*Found w\n");
+	vex_pos = G->first_vex;
+	while (vex_pos&&vex_pos->index != v)
+		vex_pos = vex_pos->nextvex;
+	if (!vex_pos)
+	{
+		printf("*the node v dont exist\n");
+		return ERROR;
+	}
+	printf("*Found v\n");
+	arcnode*arc_pos = vex_pos->first_arc;
+	if (arc_pos == NULL)
+	{
+		//dont have arc node
+		printf("*dont have arc node yet\n");
+		arc_pos = (arcnode*)malloc(sizeof(arcnode));
+		if (arc_pos == NULL)
+		{
+			printf("*overflow\n");
+			exit(OVERFLOW);
+		}
+		arc_pos->nextarc = NULL;
+		arc_pos->vex_index = w;
+		arc_pos->weight = 1;
+		vex_pos->first_arc = arc_pos;
+	}
+	else
+	{
+		while (arc_pos&&arc_pos->nextarc)
+			arc_pos = arc_pos->nextarc;
+		arc_pos->nextarc = (arcnode*)malloc(sizeof(arcnode));
+		arc_pos->nextarc->nextarc = NULL;
+		arc_pos->nextarc->vex_index = w;
+		arc_pos->weight = 1;
+	}
+	printf("*Insert arc success\n");
+	return OK;
+}
+
+status DeleteArc(GPos&G, int v, int w)
+{
+	//Graph NULL
+	if (G == NULL)
+	{
+		printf("*The Graph struct is NULL\n");
+		return ERROR;
+	}
+	if (G->first_vex == NULL)
+	{
+		printf("*The Graph is NULL\n");
+		return ERROR;
+	}
+	vexnode*vex_pos = G->first_vex;
+	while (vex_pos&&vex_pos->index != v)
+		vex_pos = vex_pos->nextvex;
+	if (!vex_pos)
+	{
+		printf("*the vex node v dont exist\n");
+		return ERROR;
+	}
+	printf("*Found v\n");
+	if (vex_pos->first_arc == NULL)
+	{
+		printf("*dont have v_w\n");
+		return ERROR;
+	}
+	arcnode*arc_pos = vex_pos->first_arc;
+	while (arc_pos&&arc_pos->vex_index != w)
+		arc_pos = arc_pos->nextarc;
+	if (!arc_pos)
+	{
+		printf("*dont have v_w\n");
+		return ERROR;
+	}
+	printf("*Found v_w\n");
+	if (arc_pos == vex_pos->first_arc)
+	{
+		//the first arc node
+		vex_pos->first_arc = arc_pos->nextarc;
+		free(arc_pos);
+		arc_pos = NULL;
+		printf("*Delete arc success\n");
+		return OK;
+	}
+	arcnode*arc_pre = vex_pos->first_arc;
+	while (arc_pre&&arc_pre->nextarc&&arc_pre->nextarc->vex_index != w)
+		arc_pre = arc_pre->nextarc;
+	arc_pre->nextarc = arc_pos->nextarc;
+	free(arc_pos);
+	arc_pos = NULL;
+	printf("*Delete arc success\n");
+	return OK;
+}
+
+status DFSTraverse(GPos&G,void(*visit)(vexnode*vex))
+{
+	//Graph NULL
+	if (G == NULL)
+	{
+		printf("*The Graph struct is NULL\n");
+		return ERROR;
+	}
+	if (G->first_vex == NULL)
+	{
+		printf("*The Graph is NULL\n");
+		return ERROR;
+	}
+	int i = 0;
+	while (i < 100)
+	{
+		visited[i] = 0;
+		i++;
+	}
+	printf("*from ? vex node begin:");
+	int begin_index = 0;
+	scanf("%d", &begin_index);
+	vexnode*vex_pos = G->first_vex;
+	while (vex_pos&&vex_pos->index != begin_index)
+		vex_pos = vex_pos->nextvex;
+	if (vex_pos == NULL)
+	{
+		printf("*input wrong,the vex node dont exist\n");
+		return ERROR;
+	}
+	DFS(G, vex_pos, visit);
+	printf("*DFS Traverse success\n");
+	return OK;
+}
+
+status BFSTraverse(GPos&G, void(*visit)(vexnode*vex))
+{
+	//Graph NULL
+	if (G == NULL)
+	{
+		printf("*The Graph struct is NULL\n");
+		return ERROR;
+	}
+	if (G->first_vex == NULL)
+	{
+		printf("*The Graph is NULL\n");
+		return ERROR;
+	}
+	int i = 0;
+	while (i < 100)
+	{
+		visited[i] = 0;
+		i++;
+	}
+	i = 0;
+	while (i < 100)
+	{
+		Queue[i] = NULL;
+		i++;
+	}
+	BFS(G, visit);
+	printf("*BFS Traverse success\n");
+	return OK;
+}
+
+status print_AdjList(GPos&G)
+{
+	//Graph NULL
+	if (G == NULL)
+	{
+		printf("*The Graph struct is NULL\n");
+		return ERROR;
+	}
+	if (G->first_vex == NULL)
+	{
+		printf("*The Graph is NULL\n");
+		return ERROR;
+	}
+	vexnode*vex_pos = G->first_vex;
+	arcnode*arc_pos = NULL;
+	while (vex_pos)
+	{
+		printf("%d->", vex_pos->index);
+		arc_pos = vex_pos->first_arc;
+		while (arc_pos)
+		{
+			printf("%d->", arc_pos->vex_index);
+			arc_pos = arc_pos->nextarc;
+		}
+		printf("\n");
+		vex_pos = vex_pos->nextvex;
+	}
+	printf("*print AdjList success\n");
+	return OK;
+}
+
+void DeleteLink(arcnode*head)
+{
+	if (head == NULL)return;
+	arcnode *temp = NULL;
+	arcnode *travel = head;
+	do
+	{
+		temp = travel->nextarc;
+		free(travel);
+		travel = temp;
+	} while (travel);
+	return;
+}
+
+void DFS(GPos&G, vexnode*vex, void(*visit)(vexnode*vex))
+{
+	visited[vex->index] = 1;
+	visit(vex);
+	arcnode*arc_pos = vex->first_arc;
+	vexnode*vex_pos = NULL;
+	while (arc_pos)
+	{
+		if (visited[arc_pos->vex_index] == 0)
+		{
+			vex_pos = G->first_vex;
+			while (vex_pos&&vex_pos->index != arc_pos->vex_index)
+				vex_pos = vex_pos->nextvex;
+			if (vex_pos == NULL)
+			{
+				printf("*the Graph is wrong\n");
+				exit(-1);
+			}
+			DFS(G, vex_pos, visit);
+		}
+		arc_pos = arc_pos->nextarc;
+	}
+}
+
+void BFS(GPos&G, void(*visit)(vexnode*vex))
+{
+	int head = 0;
+	int rear = 0;
+	vexnode*vex_pos = G->first_vex;
+	arcnode*arc_pos = NULL;
+	vexnode*vex_temp = NULL;
+	vexnode*vex_find = NULL;
+	while (vex_pos)
+	{
+		if (visited[vex_pos->index] == 0)
+		{
+			visited[vex_pos->index] = 1;
+			visit(vex_pos);
+			Queue[rear++] = vex_pos;
+			while (rear != head)
+			{
+				vex_temp = Queue[head];
+				head++;
+				arc_pos = vex_temp->first_arc;
+				while (arc_pos)
+				{
+					if (visited[arc_pos->vex_index] == 0)
+					{
+						visited[arc_pos->vex_index] = 1;
+						vex_find = G->first_vex;
+						while (vex_find&&vex_find->index != arc_pos->vex_index)
+							vex_find = vex_find->nextvex;
+						if (vex_find == NULL)
+						{
+							printf("*the Graph is wrong\n");
+							exit(-1);
+						}
+						visit(vex_find);
+						Queue[rear++] = vex_find;
+					}
+					arc_pos = arc_pos->nextarc;
+				}
+			}
+		}
+
+
+
+
+
+		vex_pos = vex_pos->nextvex;
+	}
+}
+
+void visit(vexnode*vex)
+{
+	printf("index:%d  value:%d\n", vex->index, vex->value);
+}
+
